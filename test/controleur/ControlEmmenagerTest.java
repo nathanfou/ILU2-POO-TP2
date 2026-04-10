d58cf8b1e28ad0e6823da71b1deabf7b7db37a8c
nathanfou <nathan.bayle0611@gmail.com>
2026-04-10 15:23:20 +0200

    a

--- test/controleur/ControlEmmenagerTest.java
+++ test/controleur/ControlEmmenagerTest.java
@@ -2,13 +2,27 @@ package controleur;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class ControlEmmenagerTest {
+import villagegaulois.Village;
+import personnages.Chef;
+import personnages.*;
+import histoire.*;
+import frontiere.*;
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+class ControlEmmenagerTest {
+	private Village village;
+	private Chef chef;
+
+	@BeforeEach
+	public void initialisation() {
+		System.out.println("Initialisation...\n");
+		village = new Village("Le village des irréductibles",10,5);
+		chef = new Chef("Abraracourcix",10,village);
+		village.setChef(chef);
 	}
+	
+	
 
 }
